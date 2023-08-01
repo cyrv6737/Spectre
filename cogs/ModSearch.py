e91b3455b67c297a44ff1218a4d92d3ffeae3414
cyrv6737 <cyrv6737@protonmail.com>
2023-08-01 02:32:11 -0400

    Properly implement async sleep between reactions

--- cogs/ModSearch.py
+++ cogs/ModSearch.py
@@ -83,7 +83,7 @@ class ModSearch(commands.Cog):
         reactions = ['⏮️', '◀️', '▶️', '⏭️', '❌']
         for reaction in reactions:
             await message.add_reaction(reaction)
-            sleep(0.050) # Sleep for 100ms to hopefully avoid reactions getting placed out-of-order
+            await asyncio.sleep(0.5) # Sleep for 100ms to hopefully avoid reactions getting placed out-of-order
             
         def check_react(reaction, user):
             return user == ctx.author and str(reaction.emoji) in reactions
